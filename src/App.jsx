22c8575f48204389fccc44edb9cb96f96b152558
Kirushanthi26 <kirushanthikiru26@gmail.com>
2024-03-22 16:02:40 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import Header from "./components/Header.jsx";
+import Results from "./components/Results.jsx";
+import UserInput from "./components/UserInput.jsx";
+
+function App() {
+
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
+  return (
+    <>
+      <Header />
+      {/* we use lift up -> sharing one state with 2 components =>userInput */}
+      <UserInput onChange={handleChange} userInput={userInput}/>
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
+    </>
+  );
+}
+
+export default App;
